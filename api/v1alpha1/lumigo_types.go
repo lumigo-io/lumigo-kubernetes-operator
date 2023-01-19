8cccbdc477aabef1579f0971d5e59de372509f3b
Michele Mancioppi <michelem@lumigo.io>
2023-01-19 11:07:21 +0100

    SDK init and API generation

---
+++ api/v1alpha1/lumigo_types.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2023 Lumigo.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// IMPORTANT: Run "make" to regenerate code after modifying this file
+
+// Lumigo is the Schema for the lumigoes API
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+type Lumigo struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   LumigoSpec   `json:"spec,omitempty"`
+	Status LumigoStatus `json:"status,omitempty"`
+}
+
+// LumigoList contains a list of Lumigo
+// +kubebuilder:object:root=true
+type LumigoList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Lumigo `json:"items"`
+}
+
+// LumigoSpec defines the desired state of Lumigo
+type LumigoSpec struct {
+	// The Lumigo token to be used to authenticate against Lumigo.
+	// For info on how to retrieve your Lumigo token, refer to:
+	// https://docs.lumigo.io/docs/lumigo-tokens
+	LumigoToken Credentials `json:"credentials,omitempty"`
+}
+
+type Credentials struct {
+	// Reference to a Kubernetes secret that contains the credentials
+	// for Lumigo. The secret must be in the same namespace as the
+	// LumigoSpec referencing it.
+	SecretRef KubernetesSecretRef `json:"secretRef,omitempty"`
+}
+
+type KubernetesSecretRef struct {
+	// Name of a Kubernetes secret.
+	Name string `json:"name"`
+	// Key of the Kubernetes secret that contains the credential data.
+	Key string `json:"key,omitempty"`
+}
+
+// LumigoStatus defines the observed state of Lumigo
+type LumigoStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+func init() {
+	SchemeBuilder.Register(&Lumigo{}, &LumigoList{})
+}
