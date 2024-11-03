4572e0919e34bd78f610cc053d38eb35be86350b
CircleCI <no-reply@build.com>
2024-11-03 13:40:40 +0200

    fix test III

--- tests/kubernetes-distros/kind/lumigooperator_logs_test.go
+++ tests/kubernetes-distros/kind/lumigooperator_logs_test.go
@@ -378,13 +378,11 @@ func TestLumigoOperatorLogsEventsAndObjects(t *testing.T) {
 						t.Fatalf("wrong 'k8s.cluster.name' value found: '%s'; expected: '%s'; %+v", actualClusterName, expectedClusterName, resourceAttributes)
 					}
 
-					// TODO: remove this when logs collected from pods are enriched with the cluster UID as well
-					if exportRequest.Logs().ResourceLogs().AppendEmpty().ScopeLogs().AppendEmpty().Scope().Name() == "lumigo-operator.log_file_collector" {
-						continue
-					}
-
 					if actualClusterUID, found := resourceAttributes["k8s.cluster.uid"]; !found {
+						// TODO: remove when logs collected from files have the cluster UID
+						if resourceLogs.ScopeLogs().At(0).Scope().Name() != "lumigo-operator.log_file_collector" {
 						t.Fatalf("found logs without the 'k8s.cluster.uid' resource attribute: %+v", resourceAttributes)
+						}
 					} else if actualClusterUID != expectedClusterUID {
 						t.Fatalf("wrong 'k8s.cluster.uid' value found: '%s'; expected: '%s'; %+v", actualClusterUID, expectedClusterUID, resourceAttributes)
 					}
