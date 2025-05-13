ee669fca35c14c85b1d67600c65b697122f7c748
CircleCI <no-reply@build.com>
2025-05-13 20:24:35 +0300

    WIP - events scraper

--- telemetryproxy/docker/entrypoint.sh
+++ telemetryproxy/docker/entrypoint.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 readonly OTELCOL_CONFIG_FILE_PATH="/lumigo/etc/otelcol/config.yaml"
-readonly OTELCOL_CONFIG_TEMPLATE_FILE_PATH="/lumigo/etc/otelcol-config.yaml.tpl"
+readonly OTELCOL_CONFIG_TEMPLATE_FILE_PATH=${OTELCOL_CONFIG_TEMPLATE_FILE_PATH:-"/lumigo/etc/otelcol-config.yaml.tpl"}
 readonly GENERATION_CONFIG_FILE_PATH="/lumigo/etc/otelcol/generation-config.json"
 readonly NAMESPACES_FILE_PATH="/lumigo/etc/namespaces/namespaces_to_monitor.json"
 readonly NAMESPACES_FILE_SHA_PATH="${NAMESPACES_FILE_PATH}.sha1"
