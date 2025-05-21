b801f2b132e1b49c4e706d9a8208d9d6a30caadb
Harel Moshe <harelm@lumigo.io>
2025-05-21 14:56:09 +0300

    feat: allow scaling telemetry-proxy (#153)
    Co-authored-by: CircleCI <no-reply@build.com>

--- tests/kubernetes-distros/kind/internal/install_operator.go
+++ tests/kubernetes-distros/kind/internal/install_operator.go
@@ -59,6 +59,8 @@ func installLumigoOperator(ctx context.Context, client klient.Client, kubeconfig
 		helm.WithArgs(fmt.Sprintf("--set controllerManager.manager.image.tag=%s", controllerImageTag)),
 		helm.WithArgs(fmt.Sprintf("--set controllerManager.telemetryProxy.image.repository=%s", telemetryProxyImageName)),
 		helm.WithArgs(fmt.Sprintf("--set controllerManager.telemetryProxy.image.tag=%s", telemetryProxyImageTag)),
+		helm.WithArgs(fmt.Sprintf("--set telemetryProxy.image.repository=%s", telemetryProxyImageName)),
+		helm.WithArgs(fmt.Sprintf("--set telemetryProxy.image.tag=%s", telemetryProxyImageTag)),
 		helm.WithArgs(fmt.Sprintf("--set watchdog.image.repository=%s", watchdogImageName)),
 		helm.WithArgs(fmt.Sprintf("--set watchdog.image.tag=%s", watchdogImageTag)),
 		helm.WithArgs(fmt.Sprintf("--set endpoint.otlp.url=%s", otlpSinkUrl)),
@@ -73,7 +75,7 @@ func installLumigoOperator(ctx context.Context, client klient.Client, kubeconfig
 		helm.WithArgs("--set clusterCollection.metrics.essentialOnly=true"),                          // Collect only metrics essential for the k8s page in the platform, to test this feature
 		helm.WithArgs("--debug"), // Helm debug output on install
 		helm.WithWait(),
-		helm.WithTimeout("3m"),
+		helm.WithTimeout("4m"),
 	); err != nil {
 		return ctx, fmt.Errorf("failed to invoke helm install operation due to an error: %w", err)
 	}
